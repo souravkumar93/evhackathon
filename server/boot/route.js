5d2ec311e19f7e27807fd7a61e599ff4adc132c9
souravkumar93 <Sourav_Kumar09@ad.infosys.com>
2016-08-19 23:11:24 +0530

    routes changed

--- server/boot/route.js
+++ server/boot/route.js
@@ -2,28 +2,5 @@ var path = require('path');
 
 module.exports = function(app) {
 
-    app.get('/', function(req, res) {
-        console.log("/")
-        res.redirect('/index')
-    });
-
-    app.get('/index', function(req, res) {
-        console.log("/index")
-        res.sendFile('index.html', {
-            root: path.join(__dirname, '../../client')
-        });
-    });
-
-    app.get('/login', function(req, res) {
-        console.log("/login")
-        res.sendFile('login.html', {
-            root: path.join(__dirname, '../../client')
-        });
-    });
-
-    app.get('/signup', function(req, res) {
-        res.sendFile('signup.html', {
-            root: path.join(__dirname, '../../client')
-        });
-    });
+   
 };
